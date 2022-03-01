d4d73b3cf55818fa7322fb4084226c621cffe8b0
Tori Bromma <toribromma@gmail.com>
2022-02-28 18:54:37 -0800

    scraped to the indivdual <a> element

--- server.js
+++ server.js
@@ -4,23 +4,41 @@ const puppeteer = require("puppeteer");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto("https://offerup.com");
-  //   await page.click($("div[name=search"));
-  //   await page.screenshot({ path: 'example.png' });
+app.use(express.json());
 
-  const [response] = await Promise.all([
-    await page.waitForSelector("input[name=search"),
-    await page.click("input[name=search"),
-    await page.keyboard.type("PS5"),
-    await page.keyboard.press("Enter")
-    
-  ]);
+app.get("/", (req, res) => {
+  res.send("Welcome to Offer Up Scrapper API");
+});
 
-//   console.log[response];
-//    await page.keyboard.type("input[name=search]", "PS5")
+app.listen(PORT);
 
-//   await browser.close();
-})();
+scraperObject = {
+  url: "https://offerup.com",
+  async scraper(url, query) {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto("https://offerup.com");
+    const [response] = await Promise.all([
+      await page.waitForSelector("input[name=search"),
+      await page.click("input[name=search"),
+      await page.keyboard.type(query),
+      await page.keyboard.press("Enter"),
+      await page.waitForSelector("main"),
+      
+    ]);
+
+    // await page.waitForNavigation();
+    // console.log(response)
+    await page.waitForSelector("main>div:last-child>div>div:first-child>div");
+    // var results = []
+    console.log("hi")
+    let results = await page.$$("main>div:last-child>div>div:first-child>div>a")
+    console.log(results.length);
+    // let text = await results[0].$("div:last-child>p")
+    // console.log(text)
+
+  //  await browser.close();
+
+  },
+}
+scraperObject.scraper(this.url, "PS5")
